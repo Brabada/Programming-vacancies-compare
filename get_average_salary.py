9dd7027502ab885927f6145823a02128023d553e
Brabada <freemindobject@gmail.com>
2022-10-05 15:27:05 +0300

    Refactored code

--- get_average_salary.py
+++ get_average_salary.py
@@ -1,6 +1,5 @@
 import os
 import logging
-from pprint import pprint
 
 import requests
 from terminaltables import AsciiTable
@@ -10,23 +9,15 @@ from dotenv import load_dotenv
 def fetch_vacancies_page_hh(language, page, area_id, period):
     """Returns certain page with vacancies"""
 
-    hh_base_api_url = "https://api.hh.ru/"
-    hh_vacancies_url = hh_base_api_url + "vacancies/"
-    logging.debug(hh_vacancies_url)
-    if period:
-        params = {
+    base_api_url = "https://api.hh.ru/"
+    hh_vacancies_url = base_api_url + "vacancies/"
+
+    params = {
             "text": f"Программист {language}",
             "area": area_id,
             "page": page,
             "per_page": 100,    # Number of vacancy per page
             "period": period,
-        }
-    else:
-        params = {
-            "text": "Программист",
-            "page": page,
-            "per_page": 100,
-            "area": area_id,
         }
 
     response = requests.get(hh_vacancies_url, params=params)
@@ -40,30 +31,42 @@ def fetch_vacancies_by_language(language, area_id=113, period=None):
     Returns dict with all pages with vacancies accordingly language
     In:
     language - programming language,
-    area_id - area_id from hh.ru API's list of area(default 113 - Russia)
+    area_id - area_id from hh.ru API list of area (default 113 - Russia)
     period - period for vacancies
     Out:
     Dict with vacancies data
     """
+    vacancies = {}
+    try:
+        vacancies = fetch_vacancies_page_hh(language, 0, area_id, period)
+    except requests.exceptions.HTTPError:
+        logging.warning("Can't fetch page with vacancies from hh.ru")
 
-    vacancies = fetch_vacancies_page_hh(language, 0, area_id, period)
     page = 0
     pages = vacancies['pages']
     vacancies = []
     while page < pages:
-        vacancies.append(fetch_vacancies_page_hh(language, page, area_id,
-                                                 period))
+        try:
+            vacancies.append(fetch_vacancies_page_hh(language, page, area_id,
+                                                     period))
+        except requests.exceptions.HTTPError:
+            logging.warning("Can't fetch page with vacancies from hh.ru")
         page += 1
+
     return vacancies
 
 
-def get_vacancies_quantity(vacancies):
-    """Returns number founded vacancies"""
+def get_processed_and_average_salaries(salaries):
+    """Returns tuple with processed vacancies and average salaries"""
 
-    return vacancies[0]['found']
+    # Filtering salaries from None elements
+    salaries = [salary for salary in salaries if salary]
+    if not salaries:
+        return 0, 0
+    return len(salaries), sum(salaries) // len(salaries)
 
 
-def get_average_salary_from_vacancies_hh(vacancies_pages):
+def get_data_from_vacancies_hh(vacancies_pages):
     """Returns processed vacancies and average salary"""
 
     salaries = []
@@ -73,13 +76,12 @@ def get_average_salary_from_vacancies_hh(vacancies_pages):
                 predict_rub_salary_hh(vacancy['salary'])
             )
 
-    # Filtering salaries from None elements
-    salaries = [salary for salary in salaries if salary]
-    return len(salaries), sum(salaries) // len(salaries)
+    return get_processed_and_average_salaries(salaries)
 
 
 def predict_salary(from_, to_):
     """Takes range from_ and to_ and returns average salary"""
+
     if not from_ and not to_:
         return None
     if from_ and not to_:
@@ -90,7 +92,7 @@ def predict_salary(from_, to_):
 
 
 def predict_rub_salary_hh(vacancy_salary):
-    """Returns average salary by vacance_salary dict"""
+    """Returns avg salary or None if currency is rubles or undefined salary"""
 
     if not vacancy_salary:
         return None
@@ -100,59 +102,53 @@ def predict_rub_salary_hh(vacancy_salary):
     return predict_salary(from_, to_)
 
 
-def fetch_langs_info_hh(area_id, period):
-    languages = [
-        'JavaScript',
-        'Java',
-        'Python',
-        'Ruby',
-        'PHP',
-        'C++',
-        'C#',
-        'C',
-        'Go',
-        'Objective-C',
-        'Scala',
-        'Swift',
-        'TypeScript'
-    ]
-    languages_info = {}
+def fetch_langs_data_hh(area_id, period, languages):
+    """
+    Gets area_id, period and languages for searching and return langs data
+    In:
+    area_id - id of town from https://api.hh.ru/areas
+    period - search periods in days
+    languages - list of programming languages for search
+    Out:
+    Dict with programming languages with dicts that contains fetched data for
+    this language such as:
+        vacancies_found,
+        vacancies_processed,
+        average_salary
+    """
 
-    # По каждому языку получаем кол-во найденных вакансий, обработанных
-    # вакансий и среднюю зарплату
+    languages_data = {}
+    # For every language gets count of finded vacancies, processed vacancies
+    # and average salary
     for language in languages:
-        # Загружаем вакансии по конкретному языку в vacancies
-        try:
-            vacancies = fetch_vacancies_by_language(language, area_id, period)
-        except requests.exceptions.HTTPError:
-            logging.warning('Не удалось получить список вакансий с hh.ru')
-        # Расчёт средней зп и обработанных вакансий из vacancies
+        vacancies = fetch_vacancies_by_language(language, area_id, period)
+
         vacancies_processed, average_salary = \
-            get_average_salary_from_vacancies_hh(vacancies)
-        # Добавление информации по языку в список информации по языкам
-        languages_info[language] = {
-            "vacancies_found": get_vacancies_quantity(vacancies),
+            get_data_from_vacancies_hh(vacancies)
+
+        languages_data[language] = {
+            "vacancies_found": vacancies[0]['found'],
             "vacancies_processed": vacancies_processed,
             "average_salary": average_salary
         }
 
-    return languages_info
+    return languages_data
 
 
 def predict_rub_salary_from_sj(vacancy):
-    #currency - rub
-    #payment_from
-    #payment_to
+    """Returns avg salary or None if currency is rubles or undefined salary"""
+
     if not vacancy:
         return None
     if vacancy['currency'] != 'rub':
         return None
-
     from_, to_ = vacancy['payment_from'], vacancy['payment_to']
     return predict_salary(from_, to_)
 
 
-def fetch_vacancies_page_sj(api_key, lang, town=4, page=0):
+def fetch_vacancies_page_sj(api_key, lang, town, page):
+    """Returns certain page with vacancies"""
+
     headers = {
         'X-Api-App-Id': api_key,
     }
@@ -175,19 +171,29 @@ def fetch_vacancies_page_sj(api_key, lang, town=4, page=0):
 
 def fetch_vacancies_by_language_sj(api_key, language, area_id=4):
     """
-    Returns dict with vacancies accordingly language
+    Returns dict of pages with vacancies accordingly language
     In:
-    language - programming language,
-    area_id - town from superjob.ru API's list of area(default 4 - Moscow)
+    api_key - superjob api secret key for application
+    language - programming language
+    area_id - town from superjob.ru API's list of area (default 4 - Moscow)
     Out:
     Dict with vacancies data
     """
 
     vacancies_by_lang = []
     page = 0
+
+    # Fetched dict of page have key more, that False if there is no other
+    # pages to fetch
     while True:
-        vacancies_page = fetch_vacancies_page_sj(api_key, language, area_id,
-                                                 page)
+        vacancies_page = {}
+        try:
+            vacancies_page = fetch_vacancies_page_sj(
+                api_key, language, area_id, page
+            )
+        except requests.exceptions.HTTPError:
+            logging.warning("Can't fetch page with vacancies from superjob.ru")
+
         vacancies_by_lang.append(vacancies_page)
         if not vacancies_page['more']:
             break
@@ -196,7 +202,7 @@ def fetch_vacancies_by_language_sj(api_key, language, area_id=4):
     return vacancies_by_lang
 
 
-def get_average_salary_from_vacancies_sj(vacancies_pages):
+def get_data_from_vacancies_sj(vacancies_pages):
     """Returns processed vacancies from superjob.ru and average salary"""
 
     salaries = []
@@ -206,15 +212,79 @@ def get_average_salary_from_vacancies_sj(vacancies_pages):
                 predict_rub_salary_from_sj(vacancy)
             )
 
-    # Filtering salaries from None elements
-    salaries = [salary for salary in salaries if salary]
-    # Checking for empty salaries if salaries contains before only None
-    if not salaries:
-        return 0, 0
-    return len(salaries), sum(salaries) // len(salaries)
+    return get_processed_and_average_salaries(salaries)
 
 
-def fetch_langs_info_sj(api_key, area_id):
+def fetch_langs_data_sj(api_key, area_id, languages):
+    """
+    Gets api, area_id and languages for searching and return langs data
+    In:
+    api_key - secret key for application from https://api.superjob.ru/register
+    area_id - id of town from https://api.superjob.ru/2.0/towns/
+    languages - list of programming languages for search
+    Out:
+    Dict with programming languages with dicts that contains fetched data for
+    this language such as:
+        vacancies_found,
+        vacancies_processed,
+        average_salary
+    """
+
+    languages_data = {}
+    # For every language gets count of finded vacancies, processed vacancies
+    # and average salary
+    for language in languages:
+        vacancies = \
+            fetch_vacancies_by_language_sj(api_key, language, area_id)
+        vacancies_processed, average_salary = \
+            get_data_from_vacancies_sj(vacancies)
+
+        languages_data[language] = {
+            "vacancies_found": vacancies[0]["total"],
+            "vacancies_processed": vacancies_processed,
+            "average_salary": average_salary
+        }
+
+    return languages_data
+
+
+def print_ascii_table(title, languages_data):
+    data = [[
+        "Язык программирования",
+        "Вакансий найдено",
+        "Вакансий обработано",
+        "Средняя зарплата"
+    ]]
+
+    for language, info in languages_data.items():
+        data.append(
+            [
+                language,
+                info['vacancies_found'],
+                info['vacancies_processed'],
+                info['average_salary']
+            ]
+        )
+    table = AsciiTable(data, title)
+    print(table.table)
+
+
+def print_table_of_avg_salaries_hh(languages_data):
+    title = 'HeadHunter Moscow'
+    print_ascii_table(title, languages_data)
+
+
+def print_table_of_avg_salaries_sj(languages_data):
+    title = 'SuperJob Moscow'
+    print_ascii_table(title, languages_data)
+
+
+def main():
+    load_dotenv()
+    hh_moscow_id = os.getenv('MOSCOW_CITY_ID')
+    superjob_api_key = os.getenv('SUPERJOB_API_KEY')
+    sj_moscow_id = os.getenv('SJ_MOSCOW_CITY_ID')
+
     languages = [
         'JavaScript',
         'Java',
@@ -231,75 +301,13 @@ def fetch_langs_info_sj(api_key, area_id):
         'TypeScript'
     ]
 
-    languages_info = {}
-
-    # По каждому языку получаем кол-во найденных вакансий, обработанных
-    # вакансий и среднюю зарплату
-    for language in languages:
-        # Загружаем вакансии по конкретному языку в vacancies
-        vacancies ={}
-        try:
-            vacancies = fetch_vacancies_by_language_sj(api_key,language,
-                                                       area_id)
-        except requests.exceptions.HTTPError:
-            logging.warning('Не удалось получить список вакансий с '
-                            'superjob.ru')
-        # Расчёт средней зп и обработанных вакансий из vacancies
-        vacancies_processed, average_salary = \
-            get_average_salary_from_vacancies_sj(vacancies)
-        # Добавление информации по языку в список информации по языкам
-        languages_info[language] = {
-            "vacancies_found": vacancies[0]["total"],
-            "vacancies_processed": vacancies_processed,
-            "average_salary": average_salary
-        }
-
-    return languages_info
-
-
-def print_ascii_table(title, languages_info):
-    data = []
-    data.append(
-        [
-            "Язык программирования",
-            "Вакансий найдено",
-            "Вакансий обработано",
-            "Средняя зарплата"
-        ]
+    print_table_of_avg_salaries_hh(
+        fetch_langs_data_hh(hh_moscow_id, 30, languages)
     )
-
-    for language, info in languages_info.items():
-        data.append(
-            [
-                language,
-                info['vacancies_found'],
-                info['vacancies_processed'],
-                info['average_salary']
-            ]
-        )
-    table = AsciiTable(data, title)
-    print(table.table)
-
-def print_table_of_avg_salaries_hh(languages_info):
-    title = 'HeadHunter Moscow'
-    print_ascii_table(title, languages_info)
-
-
-def print_table_of_avg_salaries_sj(languages_info):
-    title = 'SuperJob Moscow'
-    print_ascii_table(title, languages_info)
-
-
-def main():
-    load_dotenv()
-    hh_moscow_id = os.getenv('MOSCOW_CITY_ID')
-    superjob_api_key = os.getenv('SUPERJOB_API_KEY')
-    sj_moscow_id = os.getenv('SJ_MOSCOW_CITY_ID')
-
-    print_table_of_avg_salaries_hh(fetch_langs_info_hh(hh_moscow_id, 30))
     print_table_of_avg_salaries_sj(
-        fetch_langs_info_sj(superjob_api_key, sj_moscow_id)
+        fetch_langs_data_sj(superjob_api_key, sj_moscow_id, languages)
     )
+
 
 if __name__ == "__main__":
     main()
